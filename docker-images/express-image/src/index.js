9e00656c82aa0c27036beb726fb0089c9ccbe09a
Alexandre Piveteau <alexandre@piveteau.email>
2020-05-21 14:36:00 +0200

    Add an Express.js dynamic app

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,31 @@
+var Chance = require('chance');
+var Express = require('express');
+
+var chance = new Chance();
+var app = new Express();
+
+app.get('/', function(req, res) {
+  res.send(generateTransactions());
+});
+
+app.listen(3000, function() {
+  console.log('Listening on the port 3000');
+});
+
+function generateTransactions() {
+  var numberOfTransactions = chance.integer({min: 15, max: 25});
+  var transactions = [];
+  for (var i = 0; i < numberOfTransactions; i++) {
+    var currency = chance.currency().code;
+    var time = chance.date();
+    var amount = chance.integer({min: 100, max: 10000}) / 100.0;
+    var title = chance.sentence();
+    transactions.push({
+      currency : currency,
+      time : time,
+      amount : amount,
+      title : title
+    });
+  }
+  return transactions;
+}
