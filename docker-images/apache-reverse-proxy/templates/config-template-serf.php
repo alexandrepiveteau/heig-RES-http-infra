277f5f0ccef8d74db3fa40c35f9e21438dd2d05b
Alexandre Piveteau <alexandre@piveteau.email>
2020-05-23 15:04:57 +0200

    Implement some load balancing scripts

--- docker-images/apache-reverse-proxy/templates/config-template-serf.php
+++ docker-images/apache-reverse-proxy/templates/config-template-serf.php
@@ -7,32 +7,34 @@
   # `dynamic` servers must use round-robin load balancing.
 
   # Maps a serf line to its IP adress.
-  function serf_agent_to_ip($line)
-  {
-    return "";
+  function serf_agent_to_ip($line) {
+    return explode("\t", $line)[1];
   }
 
-  # Maps a serf line to its role.
-  function serf_agent_to_role($line)
-  {
-    return "unknown";
+  # Maps a serf line to its role. If no role can be found in this line, we
+  # return the "unknown" role instead.
+  function serf_agent_to_role($line) {
+    $exploded = explode("\t", $line);
+    if (count($exploded) >= 2) {
+      return $exploded[2];
+    } else {
+      return "unknown";
+    }
   }
 
   # A predicate that indicates if this line represents a static container.
-  function static_member($line)
-  {
-    return false;
+  function static_member($line) {
+    return serf_agent_to_role($line) == "static";
   }
 
   # A predicate that indicates if this line represents a dynamic container.
-  function dynamic_member($line)
-  {
-    return false;
+  function dynamic_member($line) {
+    return serf_agent_to_role($line) == "dynamic";
   }
 
   # Read the standard input.
   $input = stream_get_contents(STDIN);
-  $lines = explode("\n", $str);
+  $lines = explode("\n", $input);
 
   # Sort by member type.
   $static_members = array_filter($lines, "static_member");
@@ -45,10 +47,36 @@
 <VirtualHost *:80>
   ServerName labo.res.ch
 
-  ProxyPass '/api/transactions/' 'http://<?php print "$dynamic_app"?>/'
-  ProxyPassReverse '/api/transactions/' 'http://<?php print "$dynamic_app"?>/'
+  # Round-robin load balancer, with no routing cookie.
+  <Proxy "balancer://dynamic-balancer">
+    <?php
+    foreach($dynamic_ips as $ip) {
+      echo "BalancerMember \"http://";
+      echo $ip;
+      echo ":3000\"\n";
+    }
+    ?>
+  </Proxy>
+  ProxyPass "/api/transactions/" "balancer://dynamic-balancer/"
+  ProxyPassReverse "/api/transactions/" "balancer://dynamic-balancer/"
 
-  ProxyPass '/' 'http://<?php print "$static_app"?>/'
-  ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'
+  # Sticky load balancer (as long as the topology does not change too often).
+  Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
+  <Proxy "balancer://static-balancer">
+    <?php
+    $route = 0;
+    foreach($static_ips as $ip) {
+      echo "BalancerMember \"http://";
+      echo $ip;
+      echo ":80\" route=";
+      echo $route;
+      echo "\n";
+      $route = $route + 1;
+    }
+    ?>
+    ProxySet stickysession=ROUTEID
+  </Proxy>
+  ProxyPass "/" "balancer://static-balancer/"
+  ProxyPassReverse "/" "balancer://static-balancer/"
 
 </VirtualHost>
