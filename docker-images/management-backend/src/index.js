d386b68a3c572a876fdea2320d0dd58d98505ac7
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-01 16:34:18 +0200

    Add basic index.js

--- docker-images/management-backend/src/index.js
+++ docker-images/management-backend/src/index.js
@@ -1,12 +1,32 @@
 var Express = require('express');
+var Docker  = require('dockerode');
+var fs      = require('fs');
+var socket  = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
+var stats   = fs.statSync(socket);
+
+if (!stats.isSocket()) {
+  throw new Error('Are you sure the docker is running?');
+}
+
+var docker = new Docker({socketPath: socket });
 
 var app = new Express();
 
 app.get('/', function(req, res) {
-  console.log("Received a request !");
-  res.send({});
+    console.log("Received a request !");
+    res.send({});
+});
+
+app.get('/all', function(req, res) {
+    docker.listContainers({all: true}, function(err, containers) {
+        containers.forEach(function (containerInfo) {
+            console.log("Container info id: " + containerInfo.Id);
+        });
+        console.log('ALL: ' + containers.length);
+    });
+    res.send({});
 });
 
 app.listen(3000, function() {
-  console.log('Listening on the port 3000');
+    console.log('Listening on the port 3000');
 });
